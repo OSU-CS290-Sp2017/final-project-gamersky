ae6ff81d81fb653e91cb7e81ff6c39c0a64efa62
lixiaome <lixiaome@oregonstate.edu>
2017-06-05 19:39:35 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,3 +1,35 @@
 /*
  * Write your Express server in this file as described in README.md.
  */
+
+var path = require('path');
+var fs = require('fs');
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var peopleData = require('./peopleData');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/twits', function (req, res, next) {
+  console.log("get twit");
+	
+  
+  
+    var templateArgs = {
+      text: twitData.text,
+      author: twitData.author,
+      title: "Tweeter"
+	  
+    }
+	
+    res.render('twitData', templateArgs);
+  
+});
+
+app.get('*', function (req, res) {
+  //res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
